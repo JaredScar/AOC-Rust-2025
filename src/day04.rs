7ca8ae8d28d8ddd74648f95236ca6d697476ab7a
Jared Scarito <me@jaredscarito.com>
2025-12-04 13:11:25 -0500

    Refactor variable names for clarity in day 4 solution and comment out unused modules in main.rs

--- src/day04.rs
+++ src/day04.rs
@@ -8,12 +8,12 @@ fn check_adjacent_sideways(grid: &Vec<Vec<String>>, current_row: i32, current_co
         if left_col < 0 || right_col >= grid[current_row as usize].len() as i32 {
             break;
         }
-        let chrLeft = &grid[current_row as usize][left_col as usize];
-        let chrRight = &grid[current_row as usize][right_col as usize];
-        if chrLeft == "@" {
+        let chr_left = &grid[current_row as usize][left_col as usize];
+        let chr_right = &grid[current_row as usize][right_col as usize];
+        if chr_left == "@" {
             adjacent += 1;
         }
-        if chrRight == "@" {
+        if chr_right == "@" {
             adjacent += 1;
         }
         if adjacent > max_adjacent {
@@ -30,12 +30,12 @@ fn check_adjacent_vertically(grid: &Vec<Vec<String>>, current_row: i32, current_
         if above_row < 0 || below_row >= grid.len() as i32 {
             break;
         }
-        let chrAbove = &grid[above_row as usize][current_col as usize];
-        let chrBelow = &grid[below_row as usize][current_col as usize];
-        if chrAbove == "@" {
+        let chr_above = &grid[above_row as usize][current_col as usize];
+        let chr_below = &grid[below_row as usize][current_col as usize];
+        if chr_above == "@" {
             adjacent += 1;
         }
-        if chrBelow == "@" {
+        if chr_below == "@" {
             adjacent += 1;
         }
         if adjacent > max_adjacent {
@@ -47,24 +47,32 @@ fn check_adjacent_vertically(grid: &Vec<Vec<String>>, current_row: i32, current_
 fn check_adjacent_diagonally(grid: &Vec<Vec<String>>, current_row: i32, current_col: i32, max_adjacent: i32, check_spots: i32) -> i32 {
     let mut adjacent = 0;
     for i in 1..check_spots {
-        let above_left_row = current_row - i;
-        let below_right_row = current_row + i;
-        let above_right_col = current_col + i;
-        let below_left_col = current_col - i;
-        if above_left_row < 0 || below_right_row >= grid.len() as i32 || above_right_col >= grid[current_row as usize].len() as i32 || below_left_col < 0 {
-            break;
-        }
-        let chrAboveLeft = &grid[above_left_row as usize][below_left_col as usize];
-        let chrBelowRight = &grid[below_right_row as usize][above_right_col as usize];
-        if chrAboveLeft == "@" {
-            adjacent += 1;
-        }
-        if chrBelowRight == "@" {
-            adjacent += 1;
-        }
-        if adjacent > max_adjacent {
-            return adjacent;
-        }
+       let right_col = current_col + i;
+       let left_col = current_col - i;
+       let above_row = current_row - i;
+       let below_row = current_row + i;
+       if above_row < 0 || below_row >= grid.len() as i32 || left_col < 0 || right_col >= grid[0].len() as i32 {
+           break;
+       }
+       let top_left_char = &grid[above_row as usize][left_col as usize];
+       let top_right_char = &grid[above_row as usize][right_col as usize];
+       let bottom_left_char = &grid[below_row as usize][left_col as usize];
+       let bottom_right_char = &grid[below_row as usize][right_col as usize];
+       if top_left_char == "@" {
+        adjacent += 1;
+       }
+       if top_right_char == "@" {
+        adjacent += 1;
+       }
+       if bottom_left_char == "@" {
+        adjacent += 1;
+       }
+       if bottom_right_char == "@" {
+        adjacent += 1;
+       }
+       if adjacent > max_adjacent {
+        return adjacent;
+       }
     }
     return adjacent;
 }
@@ -76,19 +84,19 @@ pub fn run() {
     let mut grid = vec![];
     // We need to check each toilet paper (@) and see if it it has less than 4 adjacent toilet papers (8 adjacent spaces)
     for line in input.lines() {
-        let mut subGrid = vec![];
+        let mut sub_grid = vec![];
         for chr in line.chars() {
-            subGrid.push(chr.to_string());
+            sub_grid.push(chr.to_string());
         }
-        grid.push(subGrid);
+        grid.push(sub_grid);
     }
     // We now have a 2D array of the toilet paper characters
     for row in 0..grid.len() {
         for col in 0..grid[row].len() {
             let chr = &grid[row][col];
-            let isToiletPaper = chr == "@";
+            let is_toilet_paper = chr == "@";
             // Now we need to traverse above, below, left, right, and diagonally to see if there are less than 4 adjacent in 8 adjacent spaces
-            if isToiletPaper {
+            if is_toilet_paper {
                 let mut adjacent: i32 = 0;
                 adjacent += check_adjacent_sideways(&grid, row as i32, col as i32, 4, 8);
                 adjacent += check_adjacent_vertically(&grid, row as i32, col as i32, 4, 8);
