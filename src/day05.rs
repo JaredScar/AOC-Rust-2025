3a1b784a7084d21af4fec296a7ba0c651e95b830
Jared Scarito <me@jaredscarito.com>
2025-12-05 12:23:04 -0500

    Day 5 part 2 completion

--- src/day05.rs
+++ src/day05.rs
@@ -3,7 +3,7 @@ use std::fs;
 pub fn run() {
     let input = fs::read_to_string("inputs/day05.txt").expect("Failed to read input file");
     let mut p1 = 0;
-    let p2 = 0;
+    let mut p2 = 0;
     let mut ingredient_ranges = vec![];
     let mut ingredient_ids = vec![];
     for line in input.lines() {
@@ -20,6 +20,23 @@ pub fn run() {
         }
     }
 
+    // Merge overlapping ranges for part 2
+    ingredient_ranges.sort_by_key(|r| r.0);
+    let mut merged_ranges: Vec<(i64, i64)> = vec![];
+    for range in ingredient_ranges {
+        if let Some(last) = merged_ranges.last_mut() {
+            if range.0 <= last.1 + 1 {
+                // Overlapping or adjacent ranges - merge them
+                last.1 = last.1.max(range.1);
+            } else {
+                merged_ranges.push(range);
+            }
+        } else {
+            merged_ranges.push(range);
+        }
+    }
+    ingredient_ranges = merged_ranges;
+
     for ingredient_id in ingredient_ids {
         let mut is_fresh = false;
         for range in &ingredient_ranges {
@@ -31,6 +48,9 @@ pub fn run() {
         if is_fresh {
             p1 += 1;
         }
+    }
+    for range in ingredient_ranges {
+        p2 += range.1 - range.0 + 1;
     }
     println!("Part 1: {}", p1);
     println!("Part 2: {}", p2);
